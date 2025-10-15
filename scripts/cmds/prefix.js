ce66a89f1d72f83e0bb279732723fcc624221410
JAHIDUL ISLAM SAGOR <jsagor589@gmail.com>
2025-10-15 11:40:47 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -115,7 +115,7 @@ module.exports = {
         `╔══『 𝐏𝐑𝐄𝐅𝐈𝐗 』══╗\n` +
         `║ 🌍 System : ${globalPrefix}\n` +
         `║ 💬 Chatbox : ${threadPrefix}\n` +
-        `║ ➤ ${threadPrefix}help to see all available cmds` +
+        `║ ➤ ${threadPrefix}help to see all available cmds \n` +
         "╚═══════════════╝"
       );
     }
